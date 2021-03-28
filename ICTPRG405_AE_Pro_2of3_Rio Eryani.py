ba749ca9e8a6aae4673c6a14b4ad7f592d8e45ea
Rio Eryani <45652016+rioeryani@users.noreply.github.com>
2021-03-28 17:41:28 +1100

    Add files via upload

---
+++ ICTPRG405_AE_Pro_2of3_Rio Eryani.py
@@ -0,0 +1,47 @@
+
+"""
+Check for server connection. If no connection to server, show error prompt. Otherwise, continue to import csv file.
+"""
+# Import modules required for program to run.
+import os.path
+import urllib.request
+import sys
+
+# Step One: Check for main server connection. Loop until server is connected.
+"""
+try:
+    if urllib.requests.get('http://10.12.15.56').ok:
+        print("Main server is connected")
+except:
+    print("Main server is NOT connected!")
+
+
+# Step Two: Check for file server connection. Loop until server is connected.
+try:
+    if urllib.requests.get('http://10.12.15.58').ok:
+        print("File server is connected")
+except:
+    print("File server is NOT connected!")
+    sys.exit()
+
+# Step Three: Check if txt file exist in file server.
+"""
+path = "/Users/rioeryani/Google Drive/Learning/TAFE/Scripting for Cyber Security/" # This file path is temporary to test code.
+if os.path.isfile('filename.txt'):
+    f = open("filename.txt", 'r') # open file to read.
+    content = f.read()  # Display content of file.
+    print(content)
+    f.close()   # Close file after read.
+else:
+    print ("Missing customer name file! Please input text file to read.")   # Display warning if no file exist.
+
+# Create new file directory from imported txt file.
+
+with open('filename.txt') as file_name:
+    for line in file_name:
+        print(line)
+        line = line.strip() #to remove question mark at the end of name from appearing.
+        os.mkdir(os.path.join(path, line))
+
+
+# How do I display the file exist error here??
